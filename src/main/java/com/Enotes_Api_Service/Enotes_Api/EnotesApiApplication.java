8ceda21ba841c571c78545b1171901ad99970fd1
VTNhan <vtn12032004@gmail.com>
2025-10-24 22:34:49 +0700

    init-project

---
+++ src/main/java/com/Enotes_Api_Service/Enotes_Api/EnotesApiApplication.java
@@ -0,0 +1,13 @@
+package com.Enotes_Api_Service.Enotes_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnotesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnotesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Enotes_Api_Service/Enotes_Api/EnotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Enotes_Api_Service.Enotes_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnotesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
