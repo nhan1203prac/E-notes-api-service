0004230ac5c6d4efca0e076e58a63af1adedcffe
VTNhan <vtn12032004@gmail.com>
2025-11-22 13:10:38 +0700

    implement logback file

--- src/main/java/com/Enotes_Api_Service/Enotes_Api/service/Iml/HomeServiceIml.java
+++ src/main/java/com/Enotes_Api_Service/Enotes_Api/service/Iml/HomeServiceIml.java
@@ -6,17 +6,21 @@ import com.Enotes_Api_Service.Enotes_Api.exception.ResourceNotfoundException;
 import com.Enotes_Api_Service.Enotes_Api.exception.SuccessException;
 import com.Enotes_Api_Service.Enotes_Api.repository.UserRepository;
 import com.Enotes_Api_Service.Enotes_Api.service.HomeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class HomeServiceIml implements HomeService {
     @Autowired
     private UserRepository userRepository;
     @Override
     public Boolean verifyAccount(Integer userId, String verificationCode) throws ResourceNotfoundException {
+        log.info("HomeServiceImpl : verifyAccount() : Start");
         User user = userRepository.findById(userId).orElseThrow(()->new ResourceNotfoundException("invalid user"));
         if(user.getStatus().getVerificationCode() == null) {
+            log.info("message : Account already verified");
             throw new SuccessException("Account already verified");
         }
         if(user.getStatus().getVerificationCode().equals(verificationCode)){
@@ -24,7 +28,7 @@ public class HomeServiceIml implements HomeService {
             status.setVerificationCode(null);
             status.setIsActive(true);
             userRepository.save(user);
-
+            log.info("message : Account verified successfully");
             return true;
         }
         return false;
